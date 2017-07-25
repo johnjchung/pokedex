3c99f2980d9188566c70702e4244a92aad112bc7
John <johnjchung@gmail.com>
2017-07-24 20:45:07 -0400

    Added new view controller

---
+++ Pokedex/PokemonDetailViewController.h
@@ -0,0 +1,13 @@
+//
+//  PokemonDetailViewController.h
+//  Pokedex
+//
+//  Created by John Chung on 7/24/17.
+//  Copyright © 2017 John Chung. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface PokemonDetailViewController : UIViewController
+
+@end
